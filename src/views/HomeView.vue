4227c595ab0b8d7aff969224c787df945531fcbd
Brian Bugh <438465+bbugh@users.noreply.github.com>
2024-07-10 17:00:13 -0500

    Cleanup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,104 +1,52 @@
 <script setup lang="ts">
-import data from '@/data.json'
-import { pickRandomElementsInOrder, pickRandomElement, shuffleArray } from '@/utils'
-
 import WordBox from '@/components/WordBox.vue'
-import { computed, ref } from 'vue'
-
-// const difficulty = ref(3)
-
 import { useGameStore } from '@/stores/game'
 
 const store = useGameStore()
 store.newGame()
 
-// const allCategories = Object.keys(data.adjectives) as unknown as (keyof typeof data.adjectives)[]
-
-// // randomInteger(1, categories.length)
-// const adjectiveCategories = pickRandomElementsInOrder(allCategories, store.difficulty)
-// const correctAdjectives = adjectiveCategories.map((category) =>
-//   pickRandomElement(data.adjectives[category as keyof typeof data.adjectives])
-// )
-
-// const adjectives = ref(shuffleArray(correctAdjectives))
-
-// function getNoun() {
-//   return data.nouns[Math.floor(Math.random() * data.nouns.length)]
-// }
-
-// const noun = getNoun()
-
-// const guessSlots = ref<string[]>(Array(adjectives.value.length).fill(''))
-
-// const guessIsCorrect = computed(() => guessSlots.value.join('') === correctAdjectives.join(''))
-
-function startDrag(evt: DragEvent, item: string) {
-  if (!evt.dataTransfer) return
+function startDrag(event: DragEvent, item: string) {
+  if (!event.dataTransfer) return
 
   console.log('dragging')
 
+  if (!(event?.target instanceof HTMLElement)) {
+    throw new Error('target is not HTMLElement')
+  }
+
   event?.target?.classList.add('dragging')
 
-  evt.dataTransfer.dropEffect = 'move'
-  evt.dataTransfer.effectAllowed = 'move'
-  evt.dataTransfer.setData('itemID', item)
+  event.dataTransfer.dropEffect = 'move'
+  event.dataTransfer.effectAllowed = 'move'
+  event.dataTransfer.setData('itemID', item)
 }
 
-function onDragEnd(evt: DragEvent) {
-  console.log('drag end', evt?.dataTransfer?.dropEffect)
+function onDragEnd(event: DragEvent) {
+  console.log('drag end', event?.dataTransfer?.dropEffect)
+  if (!(event?.target instanceof HTMLElement)) {
+    throw new Error('target is not HTMLElement')
+  }
+
   event?.target?.classList.remove('dragging')
 
-  if (evt?.dataTransfer?.dropEffect === 'move') {
-    const itemID = evt.dataTransfer.getData('itemID')
+  if (event?.dataTransfer?.dropEffect === 'move') {
+    const itemID = event.dataTransfer.getData('itemID')
     console.log('dragged move', itemID)
-
-    // adjectives.value = adjectives.value.filter((adjective) => adjective !== itemID)
-
-    // this.adjectives = this./adjectives.filter((adjective) => adjective !== itemID)
-
-    // const item = this.items.find((item) => item.id == itemID)
-    // item.list = list
   }
 }
 
-function onDrop(evt: DragEvent, index: number) {
-  if (!evt.dataTransfer) return
+function onDrop(event: DragEvent, index: number) {
+  if (!event.dataTransfer) return
 
-  const itemID = evt.dataTransfer.getData('itemID')
+  const itemID = event.dataTransfer.getData('itemID')
   console.log('dropped', itemID, index)
   store.setGuessSlot(index, itemID)
-  // guessSlots.value[index] = itemID
-  // const item = this.items.find((item) => item.id == itemID)
-  // item.list = list
 }
-// function onDrop(evt: DragEvent, list) {
-//   const itemID = evt.dataTransfer.getData('itemID')
-//   const item = this.items.find((item) => item.id == itemID)
-//   item.list = list
-// }
-
-// function dragoverHandler(ev: DragEvent) {
-//   ev.preventDefault()
-//   console.log('dragging over')
-//   if (!ev.dataTransfer) return
-//   ev.dataTransfer.dropEffect = 'move'
-// }
-// function dropHandler(ev: DragEvent) {
-//   ev.preventDefault()
-//   console.log('dropped')
-//   // Get the id of the target and add the moved element to the target's DOM
-//   // const data = ev.dataTransfer?.getData("text/plain");
-//   // if (!data) return;
-//   // (ev.target).appendChild(document.getElementById(data));
-// }
 
 function selectDifficulty(evt: Event) {
   const target = evt.target as HTMLSelectElement
   store.difficulty = parseInt(target.value)
   store.newGame()
-  // store.setDifficulty(parseInt(target.value))
-  // difficulty.value = parseInt(target.value)
-  // console.log('difficulty', difficulty.value)
 }
 </script>
 
@@ -108,7 +56,6 @@ function selectDifficulty(evt: Event) {
 
     <div class="flex flex-col gap-3 mb-5">
       Difficulty: {{ store.difficulty }}
-      <!-- <select v-model="store.difficulty"> -->
       <select @change="selectDifficulty($event)">
         <option v-for="n in 8" :key="n" :selected="store.difficulty == n + 1">{{ n + 1 }}</option>
       </select>
