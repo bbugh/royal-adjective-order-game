e77b30a0cc42ac6a2d295be3d389ea256edd4296
Brian Bugh <438465+bbugh@users.noreply.github.com>
2024-07-10 19:03:23 -0500

    About page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,34 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="flex flex-col max-w-screen-md gap-3 mx-auto containxer">
+    <h1 class="text-4xl font-bold">About this page</h1>
+    <p>
+      This is a simple game based on the
+      <a class="underline" href="https://www.grammarly.com/blog/adjective-order/" target="_blank"
+        >weird rules of English grammar
+      </a>
+      that come naturally to native English speakers but are a huge challenge for everyone else.
+    </p>
+    <p>
+      Most native English speakers don't seem to be aware that these rules exist, but can sense when
+      the words are in a &ldquo;weird&rdquo; order, e.g.:
+      <i>big round blue ball</i> vs <i>round blue big ball</i>
+    </p>
+    <p>
+      If you want to hold someone responsible for this, you can blame
+      <!--
+      determiner, quantity, opinion, size, age, shape, color, origin, qualifier
+      -->
+      the ample brilliant vast contemporary symmetrical opaque American biodegradable
+
+      <a class="underline" target="_blank" href="https://brianbugh.me">Brian Bugh</a>.
+    </p>
+    <p>I hope you enjoy it, your highness!</p>
+    <p>
+      <RouterLink to="/" class="text-purple-500 underline">Back to the Game</RouterLink>
+    </p>
   </div>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
